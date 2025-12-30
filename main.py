e7416e27484d0c34dc44bc388cb1f770bf405177
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2025-12-30 18:11:58 +0000

    Change message to perfectus

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Greetings from the Asynchronous Realm of FastAPI!"}
+    return {"perfectus": "Greetings from the Asynchronous Realm of FastAPI!"}
 
 if __name__ == "__main__":
     import uvicorn
