daab39a466455236d11c2184a7fac92654282cd2
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2026-01-01 14:32:24 +0000

    Update welcome message

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
 from main import app
 
@@ -6,4 +10,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {"message": "Hello Perfectus!"}
