8f0d1d14fc443e31d550f9d1cb3c3b7f0c8a97c0
Sunil Agarwal <130535921+sunil-singularityx@users.noreply.github.com>
2026-01-01 20:07:39 +0530

    change message to 'Hello World' (#3)
    * Update message to 'Hello World
    
    * Change message to 'Hello Perfectus!
    
    * Add a test case
    
    * Update message to 'Hello World!
    
    * Update welcome message
    
    ---------
    
    Co-authored-by: Sunil Agarwal <sunil-singularityx@users.noreply.github.com>

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Perfectus!"}
