7cdd45cfa8f6f72624bb1f45684dc20abbc5de6c
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2026-01-03 20:51:53 +0000

    Replace 'esteemed' with 'favourite

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome, esteemed user, to the FastAPI galaxy!"}
+    return {"message": "Welcome, favourite user, to the FastAPI galaxy!"}
 
 if __name__ == "__main__":
     import uvicorn
