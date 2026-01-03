e6c9811d7dbe65c33ad2b21333c475c43630020e
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2026-01-03 18:31:41 +0000

    first

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello Perfectus!"}
+    return {"message": "🚀 Welcome to the Perfectus FastAPI adventure — your creativity starts here! ✨"}
 
 if __name__ == "__main__":
     import uvicorn
