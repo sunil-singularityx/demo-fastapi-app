dd61204dfd775c328c079929885e8bf1c9e565d1
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2026-01-05 18:44:47 +0000

    Update default message to 'Hello world

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,4 +10,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, FastAPI!"}
+    assert response.json() == {"message": "Hello world!"}
