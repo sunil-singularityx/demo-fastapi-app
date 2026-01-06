18ae9e3e90def072344b51ac1a5e576acc483cf8
Sunil Agarwal <sunil-singularityx@users.noreply.github.com>
2026-01-06 20:15:33 +0000

    Revert greeting to 'hello Perfectus' and add test

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,4 +16,9 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {"message": "Hello Perfectus!"}
+
+
+def test_read_main_status_code():
+    response = client.get("/")
+    assert response.status_code == 200
